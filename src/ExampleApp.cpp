98206e26e7b2b0589169802709feb6103f95f251
Linh Dieu Dang <alinh1504@gmail.com>
2025-03-24 17:26:37 -0500

    bret's solution

--- src/ExampleApp.cpp
+++ src/ExampleApp.cpp
@@ -152,6 +152,54 @@ void ExampleApp::onRenderGraphicsContext(const VRGraphicsState &renderState) {
             indexByteSize,
             cpuIndexArray.data()  // pointer to index data
         ));
+        
+//        // BRET'S WAY: Initialize the _mesh variable with a triangle mesh uploaded to the GPU.
+//        std::vector<Mesh::Vertex> cpuVertexArray;
+//        std::vector<int> cpuIndexArray;
+//        std::vector<std::shared_ptr<Texture>> textures;
+//
+//        vec3 normal(0, 0, 1);
+//
+//        Mesh::Vertex vert;
+//        vert.position = vec3(0, 0, 0);
+//        vert.normal = normal;
+//        vert.texCoord0 = glm::vec2(0, 0);
+//        cpuVertexArray.push_back(vert);
+//        cpuIndexArray.push_back(0);
+//
+//        vert.position = vec3(1, 0, 0);
+//        vert.normal = normal;
+//        vert.texCoord0 = glm::vec2(1, 0);
+//        cpuVertexArray.push_back(vert);
+//        cpuIndexArray.push_back(1);
+//
+//        vert.position = vec3(1, 1, 0);
+//        vert.normal = normal;
+//        vert.texCoord0 = glm::vec2(1, 1);
+//        cpuVertexArray.push_back(vert);
+//        cpuIndexArray.push_back(2);
+//
+//        // std::shared_ptr<Texture> tex = Texture::create2DTextureFromFile("tex.jpg");
+//        // textures.push_back(tex);
+//
+//        const int numVertices = cpuVertexArray.size();
+//        const int cpuVertexByteSize = sizeof(Mesh::Vertex) * numVertices;
+//        const int cpuIndexByteSize = sizeof(int) * cpuIndexArray.size();
+//
+//        _mesh.reset(new Mesh(
+//            textures,
+//            GL_TRIANGLE_STRIP,
+//            GL_STATIC_DRAW,
+//            cpuVertexByteSize,
+//            cpuIndexByteSize,
+//            0,
+//            cpuVertexArray,
+//            cpuIndexArray.size(),
+//            cpuIndexByteSize,
+//            &cpuIndexArray[0]
+//        ));
+//
+//        _mesh->setMaterialColor(vec4(1, 0, 0, 1.0));
     }
 }
 
